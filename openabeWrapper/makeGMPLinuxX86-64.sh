5b95d8b61d2dc193de06b4a501e29020acc926fa
sberlato <sberlato@fbk.eu>
2022-09-27 14:30:37 +0200

    removed comment on CXX export

--- openabeWrapper/makeGMPLinuxX86-64.sh
+++ openabeWrapper/makeGMPLinuxX86-64.sh
@@ -6,7 +6,7 @@ echo "Start of make GMP Linux x86_64 script"
 
 # This is to use Kotlin toolchain, otherwise make process may mess up with libraries versions when interoping
 export CC=$HOME/.konan/dependencies/x86_64-unknown-linux-gnu-gcc-8.3.0-glibc-2.19-kernel-4.9-2/bin/x86_64-unknown-linux-gnu-gcc
-#export CXX=$HOME/.konan/dependencies/x86_64-unknown-linux-gnu-gcc-8.3.0-glibc-2.19-kernel-4.9-2/bin/x86_64-unknown-linux-gnu-g++
+export CXX=$HOME/.konan/dependencies/x86_64-unknown-linux-gnu-gcc-8.3.0-glibc-2.19-kernel-4.9-2/bin/x86_64-unknown-linux-gnu-g++
 
 cd gmp/gmp-6.2.1/
 
--- openabeWrapper/makeOpenABELinuxX86-64.sh
+++ openabeWrapper/makeOpenABELinuxX86-64.sh
@@ -11,7 +11,7 @@ sudo -E ./deps/install_pkgs.sh
 
 # This is to use Kotlin toolchain, otherwise make process may mess up with libraries versions when interoping
 export CC=$HOME/.konan/dependencies/x86_64-unknown-linux-gnu-gcc-8.3.0-glibc-2.19-kernel-4.9-2/bin/x86_64-unknown-linux-gnu-gcc
-#export CXX=$HOME/.konan/dependencies/x86_64-unknown-linux-gnu-gcc-8.3.0-glibc-2.19-kernel-4.9-2/bin/x86_64-unknown-linux-gnu-g++
+export CXX=$HOME/.konan/dependencies/x86_64-unknown-linux-gnu-gcc-8.3.0-glibc-2.19-kernel-4.9-2/bin/x86_64-unknown-linux-gnu-g++
 
 # Build OpenABE
 make
